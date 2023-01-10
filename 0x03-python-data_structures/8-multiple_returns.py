d8bdab0eedad69f54838449d3c9c1b9ff34e540c
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-10 09:08:51 -0800

    all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     lenght = len(sentence)
     my_tuple = ()
     if lenght == 0:
-        my_tuple == 0, "None"
+        my_tuple = 0, "None"
     else:
         my_tuple = lenght, sentence[0]
         return my_tuple
