52bf7602f226e60de92dd0b8b43dc7554f6fd5cb
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-10 09:01:39 -0800

    all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    my_tuple = ()
-    if length == 0:
-        my_tuple = 0, "None"
+    if len(sentence) == 0:
+        return None
     else:
-        my_tuple = length, sentence[0]
-    return my_tuple
+        return len(sentence), sentence[0]
