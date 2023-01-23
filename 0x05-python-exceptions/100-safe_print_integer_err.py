79215d8078914333ed52ccf3bf0c072560a5c68b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-23 15:31:25 -0800

    a function that divides element by element 2 lists.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 
 """
-a function that prints an integer with "{:d}".format().
-Prototype: def safe_print_integer(value):
-value can be any type (integer, string, etc.)
-The integer should be printed followed by a new line
-Returns True if value has been correctly printed
-(it means the value is an integer)
-Otherwise, returns False
+Write a function that executes a function safely.
+
+Prototype: def safe_function(fct, *args):
+You can assume fct will be always a pointer to a function
+Returns the result of the function,
+Otherwise, returns None if something happens during the function
+and prints in stderr the error precede by Exception:
 You have to use try: / except:
-You have to use "{:d}".format() to print as integer
-You are not allowed to import any module
-You are not allowed to use type()
 """
 
 
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as err: 
+    except (ValueError, TypeError) as err:
         print("Exception: {}".format(err), file=sys.stderr)
         return False
     else:
