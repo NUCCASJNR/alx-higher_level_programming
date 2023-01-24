d8bbfb0cc24eae23b4be4912386af7f346919b03
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-24 03:48:38 -0800

    an empty class Square that defines a square based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py by Al-Areef
+"""Defines a square """
+
+
+class Square:
+    """Represents the square"""
+
+    def __init__(self, size):
+        """initializing the square class
+        size - size of the square
+        """
+
+        self.__size = size
