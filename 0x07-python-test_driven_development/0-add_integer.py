81224242fd938c43c05c16c44a792b180959ab55
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-27 15:13:20 -0800

    a function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" function that adds 2 integers."""
+""" function that adds 2 integers.
+    Args:
+        a = first integer
+        b = second integer
+"""
 
 
 def add_integer(a, b=98):
-    """Adds int a & b """
+    """if a and b not int/float
+        raise TypeError
+        Return: a + b"""
     if not isinstance(a, int) | isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) | isinstance(b, float):
---
+++ 0x07-python-test_driven_development/tests/test.py
@@ -0,0 +1,9 @@
+def my_function(a, b=7):
+    """Returns a*b
+    """
+
+    return a*b
+x = my_function(4)
+
+print(x)
+print(my_function.__doc__)
