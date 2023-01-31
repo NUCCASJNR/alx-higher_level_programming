67e2fafe11d337ca2fbe230735d3d942a5849977
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-01-31 10:38:49 -0800

    update

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name."""
+
+class LockedClass:
+    """initializing the class"""
+    __slots__ = ["first_name"]
