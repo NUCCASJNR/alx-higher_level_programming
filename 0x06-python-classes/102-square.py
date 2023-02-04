05cb525f081c7790f530287ec0fbc9d3e77aaec8
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-04 10:26:37 -0800

    all

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# 4-square.py by Al-Areef
+"""A class that defines a square"""
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing the square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves value of square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """ Calculates the area of the square
+        Returns: The square of the size
+        """
+
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
