5df2d1da25f38d54b895c70c3db7f9d650edb6c1
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-06 09:49:56 -0800

    task 4

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,11 @@
 
 """Class mylist that inherits from list"""
 
+
 class MyList(list):
     """Initializing the class"""
 
     def print_sorted(self):
         """Prints the soretd list"""
 
-        print (sorted(self))
+        print(sorted(self))
