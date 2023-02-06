41df755504a43bf4ce555756ea09198e7bd1f487
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-06 09:45:44 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
     Return:
         True or False
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
         return False
