812d35971298e0f910cf2b314735906f8facd8da
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-07 07:41:56 -0800

    JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""8-class_to_json.py module"""
+
+
+def class_to_json(obj):
+    """a function that returns the dictionary description with simple data structure
+         (list, dictionary, string, integer and boolean) for JSON
+         serialization of an object
+    """
+
+    return obj.__dict__
---
+++ 0x0B-python-input_output/docstring.py
@@ -0,0 +1,5 @@
+def my_function(self):
+  """Doc string for my function."""
+  # print the Docstring here.
+  
+print(my_function.__doc__)
