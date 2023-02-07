528e173bc927e66153fc5ee5b8d6708fbe891f3f
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-07 03:55:41 -0800

    a function that returns the JSON representation of an object (string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""3-to_json_string.py module"""
+
+
+import json
+"""takes any python data type and convert it to string representation"""
+
+def to_json_string(my_obj):
+    """Initializing the function"""
+
+    return json.dumps(my_obj)
