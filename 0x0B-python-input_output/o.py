76ced55c12deb1f2ae089b3e42d1205b95da533b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-07 05:53:54 -0800

    all

---
+++ 0x0B-python-input_output/o.py
@@ -0,0 +1,10 @@
+# Python program to demonstrate
+# sys.argv
+
+
+import sys
+
+print("This is the name of the program:", sys.argv[1:])
+
+print("Argument List:", str(sys.argv))
+
---
+++ 0x0B-python-input_output/test.py
@@ -0,0 +1,25 @@
+#1/usr/bin/python3
+
+# Python program to demonstrate
+# command line arguments
+
+import sys
+
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+	print(sys.argv[i], end = " ")
+	
+# Addition of numbers
+Sum = 0
+
+for i in range(1, n):
+	Sum += int(sys.argv[i])
+	
+print("\n\nResult:", Sum)
