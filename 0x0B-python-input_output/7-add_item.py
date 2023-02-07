ce074bce0ad47446da01c5e82cb6de8a1865db4d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-07 06:57:35 -0800

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,8 @@
 import sys
 if __name__ == "__main__":
     save = __import__("5-save_to_json_file").save_to_json_file
-    load = __import__("6-load_from_json_file").load_from_json_file
+    load =\
+        __import__("6-load_from_json_file").load_from_json_file
 
     try:
         items = load("add_item.json")
