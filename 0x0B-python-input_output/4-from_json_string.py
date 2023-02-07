89dd7b79090d40060959f6b5abcfa875a58333f2
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-07 04:29:12 -0800

    update

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""4-from_json_string.py module"""
+
+
+import json
+"""The json module"""
+
+
+
+def from_json_string(my_str):
+    """a function that returns an object (Python data structure)
+    represented by a JSON string
+    Arguments:
+        my_str: the object to be returned
+    """
+
+    return json.loads(my_str)
