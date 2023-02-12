e5d99444975aec38901d56eca6a4bea74d85a0e1
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-12 12:01:13 +0100

    Still Almlost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,3 +26,23 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+
+        file_name = "{}.json".format(cls.__name__)
+        with (open(file_name, "w")) as a:
+            if list_objs is None or list_objs == []:
+                a.write("[]")
+            else:
+                list_dict = [i.to_dictionary() for i in list_objs]
+                a.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string"""
+
+        if json_string is None or json_string == []:
+            return ("[]")
+        return (json.loads(json_string))
