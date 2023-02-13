95b579467a0bee26ccb108a8a97e34033d1649cd
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-02-13 14:22:00 -0800

    Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-# test_rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines unittests for models/rectangle.py.
-
-Unittest classes:
-    TestRectangle_instantiation - line 25
-    TestRectangle_width - line 114
-    TestRectangle_height - line 190
-    TestRectangle_x - line 262
-    TestRectangle_y - line 334
-    TestRectangle_order_of_initialization - line 402
-    TestRectangle_area - line 430
-    TestRectangle_update_args - line 538
-    TestRectangle_update_kwargs - line 676
-    TestRectangle_to_dictionary - line 788
-"""
+"""Defines unittests for models/rectangle.py"""
 import io
 import sys
 import unittest
