a21dd4145242cbc7f0ecf0ea545fa7b160c32380
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-14 21:01:50 +0100

    Fix task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -14,9 +14,9 @@
 -- id = 4, name = “George”, score = 8
 
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT
-	name VARCHAR(256)
-	score INT
+	id INT,
+	name VARCHAR(256),
+	score INT,
 	)
 
 INSERT INTO second_table (id, name, score) VALUES
