7a4ec6b3d8d2386ad26c88a8e3a7e03a02df745b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-14 16:18:03 +0100

    script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -4,4 +4,4 @@
 -- The database name will be passed as an argument of the mysql command
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE TABLE first_name;
+SHOW CREATE TABLE first_table;
