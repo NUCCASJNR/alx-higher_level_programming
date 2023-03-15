b73c198cca31eb34bf6ddb26ac75579c588dcd54
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-15 02:04:58 +0100

    cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,4 +4,4 @@
 -- Write a script that displays the top 3 of cities temperature
 -- during July and August ordered by temperature (descending)
 
-SELECT city, AVG(value) AS avg_temp FROM temperatues ORDER BY DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatues ORDER BY avg_temp DESC LIMIT 3;
