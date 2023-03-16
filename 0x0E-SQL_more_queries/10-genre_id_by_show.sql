bbfc2bdb18b4c6af562e0035e9516428c6036cc9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-16 02:29:35 +0100

    Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -8,7 +8,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT DISTINCT  tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
 WHERE tv_shows.id = tv_show_genres.tv_show.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
