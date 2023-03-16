dba12f2255dbecd7e7adc5da19a7e1f24bf46d80
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-16 01:14:13 +0100

    task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -8,5 +8,4 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
