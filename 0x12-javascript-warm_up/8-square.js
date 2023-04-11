877c0cd55f73926e88a0c1a43fc65f0d675496c8
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-11 16:11:08 +0100

    prints a square with the X sign depending on the value of the arg passed to the script

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 //  Write a script that prints a square
-//
 //  The first argument is the size of the square
 //  If the first argument can’t be converted to an integer, print “Missing size”
 //  You must use the character X to print the square
