4bfb60717e38692adc671b6cd292d8d1b3b72636
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-11 16:00:30 +0100

    C, python, javascript

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// A script that prints  3lines
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
