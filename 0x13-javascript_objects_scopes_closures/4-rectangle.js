adac191e9d72fd480a9e01cf3f506672b4ed0857
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-12 21:58:47 +0100

    Add task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+// Write a class Rectangle that defines a rectangle:
+//
+// You must use the class notation for defining your class
+// The constructor must take 2 arguments w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the
+// rectangle using the character X
+// Create an instance method called rotate() that exchanges the width and the height of the rectangle
+// Create an instance method called double() that multiples the width and the height of the rectangle by 2
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
