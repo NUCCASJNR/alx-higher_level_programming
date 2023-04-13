7e0f6ce6050667653887badce61f686bfd74a56d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-13 05:58:46 +0100

    Iter

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Write a function that prints the number of arguments already printed and the new argument value. (see example below)
+//
+// Prototype: exports.logMe = function (item)
+// Output format: <number arguments already printed>: <current argument value>
+
+let iter = 0;
+exports.logMe = function (item) {
+  console.log(`${iter}: ${item}`);
+  iter++;
+}
