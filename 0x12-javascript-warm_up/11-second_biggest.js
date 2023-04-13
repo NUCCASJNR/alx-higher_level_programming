d3426c3ca3c3f724e47934a42e329b28e0420002
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-13 16:26:41 +0100

    Update second max

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,8 +11,8 @@
 
 const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
-console.log(0);
+  console.log(0);
 } else {
-const sort = args.map((ele)=>Number(ele)).sort((a, b)=>(b-a));
+const sort = args.map((ele) => Number(ele)).sort((a, b) => (b - a));
 console.log(sort[1]);
 }
