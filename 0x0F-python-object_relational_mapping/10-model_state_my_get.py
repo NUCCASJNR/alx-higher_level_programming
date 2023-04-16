5b13d4ca4dd5730c342a0324ec781145d804dae9
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-16 08:07:42 +0100

    a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all State objects that contain the letter a from
+the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State, Base
+import sys
+
+if __name__ == "__main__":
+    """
+    Create the engine
+    """
+    url = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    engine = create_engine(url)
+
+    """
+    Create a session
+    """
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """
+    Write the sql query to be executed below
+    """
+
+    state_name = sys.argv[4]
+    users = session.query(State).filter(State.name.like(
+        f'%{state_name}')).all()
+    if users:
+        for user in users:
+            print(user.id)
+    else:
+        print("Not found")
