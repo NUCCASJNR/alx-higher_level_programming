feeb8d51ff91bec97c42051318eecc1d635b3897
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-16 12:33:59 +0100

    a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+a script 14-model_city_fetch_by_state.py that prints all City
+objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+if __name__ == "__main__":
+
+    """Establish the database connection to create the engine
+    """
+
+    url = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(url)
+
+    """
+    Create the session
+    """
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """
+    Write query to be executed below
+    """
+
+    cities = session.query(City, State).join(City)
+    for city, state in cities.all():
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Python file similar to model_state.py named model_city.py that
+contains the class definition of a City.
+"""
+
+from sqlalchemy import create_engine, Integer, Column, String, Sequence, ForeignKey
+from model_state import Base, State
+
+class City(Base):
+    """
+    City class that inherits from Base
+    Links to the MySQL table cities
+    Args:
+        id: unique integer, cant be null, primary key, auto generated
+        name: String, cant be null
+        state_id: integer, foreign key
+    """
+
+    __tablename__ = 'cities'
+    id = Column(Integer, Sequence('city_id_seq'), unique=True,
+            nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
