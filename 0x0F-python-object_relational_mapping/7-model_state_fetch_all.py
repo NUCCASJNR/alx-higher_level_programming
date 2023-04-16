023ad441187971fc6e11e4c7e081f26c2de4ad41
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-16 06:12:19 +0100

    a script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
 
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///mydb.sqlite')
+if __name__ == "__main__":
+    """
+    Create the database engine
+    """
+    url = ('mysql://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    """
+    create a session to interact with the engine
+    """
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-State = session.query(State).order_by(states.id.asc()).all()
-for state in State:
-    print(state.id, state.name)
+    """
+    write the query to be executed
+    """
+    users = session.query(State).order_by(State.id)
+    for user in users:
+        print('{}: {}'.format(user.id, user.name))
