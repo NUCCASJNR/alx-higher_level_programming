b5ea90820c55121f84f065848443638b81d6c323
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-17 13:35:29 +0100

    test

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     session = Session()
 
     new_state = State(name='California')
-    new_city = City(name='San Francisco')
+    new_city = City(name='San Francisco', state_id = 2)
     new_city.states = new_state
     session.add(new_state)
     session.add(new_city)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -22,4 +22,4 @@ class State(Base):
     id = Column(Integer, Sequence('state_id_seq'), unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='delete')
+    cities = relationship('City', backref='state', cascade='all, delete')
