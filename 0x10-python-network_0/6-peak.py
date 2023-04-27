ceb0fa0728cb6c3e4ded4c920251de63b0967925
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-28 00:50:13 +0100

    try this for task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,6 @@ def find_peak(list_of_integers):
     """
     Returns peak
     """
-    if list_of_integers == []:
+    if len(list_of_integers) == 0:
         return None
     return (max(list_of_integers))
