fd930e5e60bed6c87b1c7c739fc1c00ccc39d85d
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-27 12:19:16 +0100

    fix wc -l error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/bash
-#Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-#The size must be displayed in bytes
-#You have to use curl
-
+#Bash script that takes in a URL, sends a request to that URL
 curl -s "$1":"$2"
