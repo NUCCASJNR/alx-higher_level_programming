bcb148aa50dfd52671cfed94b67a8782a18bfb48
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-28 15:11:24 +0100

    Fix failed checks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     print(f"Body response:")
-    print(f"\t- type: {type(r.content)}")
+    print(f"\t- type: {type(r.text)}")
     print(f"\t- content: {r.content.decode()}")
