0e15341a4d42fc965cebcfbf12d5356d6eb16203
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-28 12:43:33 +0100

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as r:
         content = r.read()
         print('Body response:')
-        print(f'\t - type: {type(content)}')
-        print(f'\t - content: {content}')
-        print(f'\t - utf8 content: {content.decode("utf-8")}')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
