289079cdebdc39d6828a55ec0bb412ce8833d998
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-28 15:10:02 +0100

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,9 @@ python script that fetches https://alx-intranet.hbtn.io/status
 """
 
 import requests
-
-url = "https://alx-intranet.hbtn.io/status"
-r = requests.get(url)
-print(f"Body response:")
-print(f"\t- type: {type(r.content)}")
-print(f"\t- content: {r.content.decode()}")
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(url)
+    print(f"Body response:")
+    print(f"\t- type: {type(r.content)}")
+    print(f"\t- content: {r.content.decode()}")
