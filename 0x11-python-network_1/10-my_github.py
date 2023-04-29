c2f746d95b124508d5287c726b47e1ac0cc45dbc
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-04-29 13:08:00 +0100

    update Auth

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,13 +7,14 @@ and password) and uses the GitHub API to display your id
 import sys
 import requests
 
-user_name = sys.argv[1]
-personal_token = sys.argv[2]
-Auth = {"Authorization": f"token {personal_token}"}
-url = f"https://api.github.com/users/{user_name}"
-res = requests.get(url, headers=Auth)
-if res.status_code == 200:
-    user_data = res.json()
-    print(f"{user_data.get('id')}")
-else:
-    print('None')
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    personal_token = sys.argv[2]
+    Auth = {"Authorization": f"token {personal_token}"}
+    url = f"https://api.github.com/users/{user_name}"
+    res = requests.get(url, headers=Auth)
+    if res.status_code == 200:
+        user_data = res.json()
+        print(f"{user_data.get('id')}")
+    else:
+        print('None')
