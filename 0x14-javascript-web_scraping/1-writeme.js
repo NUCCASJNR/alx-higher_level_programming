d8140e562f90070580877f109bd68a2a7c901f88
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-23 09:57:11 -0700

    a script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+// Write a script that writes a string to a file.
+
+// The first argument is the file path
+// The second argument is the string to write
+// The content of the file must be written in utf-8
+// If an error occurred during while writing, print the error object
+
+const file = require('fs');
+const write = process.argv[2];
+const content = process.argv[3];
+
+file.writeFile(`${write}`, `${content}`, 'utf8', (eror) => {
+  if (eror) {
+    console.error(eror);
+  } else {
+    console.log('File written successfully!');
+  }
+});
